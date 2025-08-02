54f3dab83a24fd5a320e8616daa1048aeb23199f
henriquedessen <56644427+HenriqueCDessen@users.noreply.github.com>
2025-08-02 17:57:21 -0300

    fechamento do leilão

--- internal/infra/database/auction/create_auction.go
+++ internal/infra/database/auction/create_auction.go
@@ -5,7 +5,11 @@ import (
 	"fullcycle-auction_go/configuration/logger"
 	"fullcycle-auction_go/internal/entity/auction_entity"
 	"fullcycle-auction_go/internal/internal_error"
+	"os"
+	"strconv"
+	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -31,6 +35,7 @@ func NewAuctionRepository(database *mongo.Database) *AuctionRepository {
 func (ar *AuctionRepository) CreateAuction(
 	ctx context.Context,
 	auctionEntity *auction_entity.Auction) *internal_error.InternalError {
+
 	auctionEntityMongo := &AuctionEntityMongo{
 		Id:          auctionEntity.Id,
 		ProductName: auctionEntity.ProductName,
@@ -40,11 +45,65 @@ func (ar *AuctionRepository) CreateAuction(
 		Status:      auctionEntity.Status,
 		Timestamp:   auctionEntity.Timestamp.Unix(),
 	}
+
 	_, err := ar.Collection.InsertOne(ctx, auctionEntityMongo)
 	if err != nil {
 		logger.Error("Error trying to insert auction", err)
 		return internal_error.NewInternalServerError("Error trying to insert auction")
 	}
 
+	go ar.scheduleAuctionClose(auctionEntity.Id, auctionEntity.Timestamp)
+
+	return nil
+}
+
+func getAuctionDuration() time.Duration {
+	secondsStr := os.Getenv("AUCTION_DURATION_SECONDS")
+	seconds, err := strconv.Atoi(secondsStr)
+	if err != nil || seconds <= 0 {
+		return 30 * time.Second // valor padrão
+	}
+	return time.Duration(seconds) * time.Second
+}
+
+func (ar *AuctionRepository) scheduleAuctionClose(auctionID string, startTime time.Time) {
+	duration := getAuctionDuration()
+	timeToClose := startTime.Add(duration).Sub(time.Now())
+	if timeToClose <= 0 {
+		timeToClose = 0
+	}
+	timer := time.NewTimer(timeToClose)
+
+	<-timer.C
+
+	err := ar.closeAuctionIfExpired(auctionID)
+	if err != nil {
+		logger.Error("Erro ao fechar leilão automaticamente", err)
+	}
+}
+
+func (ar *AuctionRepository) closeAuctionIfExpired(auctionID string) error {
+	var auction AuctionEntityMongo
+	err := ar.Collection.FindOne(context.Background(), bson.M{"_id": auctionID}).Decode(&auction)
+	if err != nil {
+		return err
+	}
+
+	if auction.Status == auction_entity.Completed {
+		return nil
+	}
+
+	now := time.Now().Unix()
+	duration := getAuctionDuration()
+	expirationTime := auction.Timestamp + int64(duration.Seconds())
+
+	if now >= expirationTime {
+		_, err := ar.Collection.UpdateOne(
+			context.Background(),
+			bson.M{"_id": auctionID},
+			bson.M{"$set": bson.M{"status": auction_entity.Completed}},
+		)
+		return err
+	}
 	return nil
 }
